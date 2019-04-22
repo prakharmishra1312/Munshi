daf0174cb2e74c6f180d3f73855e9d0b29096d17
prakharmishra1312 <30681451+prakharmishra1312@users.noreply.github.com>
2019-04-22 19:48:02 +1000

    "New User Service Impl"

--- Users/src/main/java/com/munshi/users/service/UserDAOServiceImpl.java
+++ Users/src/main/java/com/munshi/users/service/UserDAOServiceImpl.java
@@ -1,5 +1,41 @@
 package com.munshi.users.service;
 
-public class UserDAOServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.munshi.users.bean.User;
+import com.munshi.users.repository.UserRepository;
+
+@Service
+public class UserDAOServiceImpl implements UserDAOService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public User getUserById(String id) {
+		
+		return userRepository.findById(id).get();
+	}
+
+	@Override
+	public User createUser(User user) {
+		
+		return userRepository.save(user);
+	}
+
+	@Override
+	public User updateUser(User user) {
+		return userRepository.save(user);
+		
+	}
+
+	@Override
+	public User deleteUser(String id) {
+		// TODO Auto-generated method stub
+		User user=getUserById(id);
+		userRepository.delete(user);
+		return user;
+	}
 
 }
