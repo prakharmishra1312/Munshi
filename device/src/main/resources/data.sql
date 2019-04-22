a034a134c1d4ec6ec0a624cd22a508ace28be69d
prakharmishra1312 <30681451+prakharmishra1312@users.noreply.github.com>
2019-04-22 14:30:39 +1000

    Initial commit

---
+++ device/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into device (id,type, user_id) values ('testid1', 'default', 'test@dummy.com')
+insert into device (id,type, user_id) values ('testid2', 'default', 'test@dummy.com')
+insert into device (id,type, user_id) values ('testid3', 'default', 'test1@dummy.com')
